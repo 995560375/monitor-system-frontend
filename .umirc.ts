c52495881dffd7ff6f20938c084f1c94f8ef8278
Huikun Zheng <huikunzheng9955@gmail.com>
2023-07-11 08:53:56 +0800

    layout

--- .umirc.ts
+++ .umirc.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "umi";
 
 export default defineConfig({
   routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
+    { path: "/", component: "index", name: "home" },
+    { path: "/docs", component: "docs", name: "docs" },
+    // { path: "/failures", component: "failures", name: "failures"}
   ],
   npmClient: 'pnpm',
 });
