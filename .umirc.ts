59d96b3374f5ed8b50ceb37250b436185dbb3a7d
Huikun Zheng <huikunzheng9955@gmail.com>
2023-07-18 11:47:01 +0800

    talbe

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   routes: [
     { path: "/", component: "index", name: "home" },
     { path: "/docs", component: "docs", name: "docs" },
-    { path: "/failures", component: "@/components/Failures", name: "Failures"}
+    { path: "/failures", component: "@/components/Table", name: "Failures"}
   ],
   npmClient: 'pnpm',
 });
